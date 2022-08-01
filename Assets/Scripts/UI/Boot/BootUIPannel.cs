9f9516011d28b31e1fa191ac05c87a61ee250013
BILIBILIniyichao <niyichao@bilibili.com>
2022-08-01 19:06:18 +0800

    创建开始界面

---
+++ Assets/Scripts/UI/Boot/BootUIPannel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+
+namespace MyUI
+{
+    /// <summary>
+    /// 启动界面UI
+    /// </summary>
+    public class BootUIPannel : MonoBehaviour
+    {
+        public string startBtnToScene;
+
+        private UIPanel _Panel;
+        private GComponent _UIroot;
+
+        private GButton _QuitButton;
+        private GButton _StartButton;
+
+        private void Awake()
+        {
+            _Panel = GetComponent<UIPanel>();
+            _UIroot = _Panel.ui;
+
+            Assert.IsNotNull(_Panel);
+            Assert.IsNotNull(_UIroot);
+        }
+
+        private void Start()
+        {
+            _QuitButton = _UIroot?.GetChild("Button_Quit").asButton;
+            _StartButton = _UIroot?.GetChild("Button_Start").asButton;
+
+            _QuitButton?.onClick.Add(() => { UIOperationUtil.QuitGame(); });
+            _StartButton?.onClick.Add(() =>
+            {
+                UIOperationUtil.GoToScene(startBtnToScene);
+            });
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Util/UIOprationUtil.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using FairyGUI;
+
+namespace MyUI
+{
+    /// <summary>
+    /// UI操作基础类
+    /// </summary>
+    public static class UIOperationUtil
+    {
+        public static void QuitGame()
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+
+        public static void GoToScene(string scene)
+        {
+            if (!string.IsNullOrEmpty(scene))
+            {
+                SceneManager.LoadScene(scene);
+            }
+            else
+            {
+                Debug.LogWarning($"{scene} is null or empty!!!");
+            }
+        }
+    }
+}
+
