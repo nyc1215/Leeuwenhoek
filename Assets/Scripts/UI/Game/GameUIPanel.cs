e2ff511ec469b2991c72645d24df2e44c40277ef
BILIBILIniyichao <niyichao@bilibili.com>
2022-08-29 12:18:17 +0800

    移除游戏界面中关于NetCode创建服务器、主机和客户端的按钮

--- Assets/Scripts/UI/Game/GameUIPanel.cs
+++ Assets/Scripts/UI/Game/GameUIPanel.cs
@@ -9,25 +9,12 @@ namespace UI.Game
     {
         private JoyStickModule _joystick;
 
-        private GButton _startServer;
-        private GButton _startHost;
-        private GButton _startClient;
-
-
         protected override void Awake()
         {
             base.Awake();
             _joystick = new JoyStickModule(UIRoot);
             _joystick.onMove.Add(JoystickMove);
             _joystick.onEnd.Add(JoystickMove);
-
-            _startServer = GetButton("Button_Server");
-            _startHost = GetButton("Button_Host");
-            _startClient = GetButton("Button_Client");
-
-            _startServer.onClick.Add(() => { NetworkManager.Singleton.StartServer(); });
-            _startHost.onClick.Add(() => { NetworkManager.Singleton.StartHost(); });
-            _startClient.onClick.Add(() => { NetworkManager.Singleton.StartClient(); });
 
             CreatePlayer();
         }
