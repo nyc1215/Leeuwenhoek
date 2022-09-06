ce0ae09e3d1d296b879e29e4f72bf98bda3ac104
BILIBILIniyichao <niyichao@bilibili.com>
2022-09-06 18:54:19 +0800

    管理全场任务的单例类创建

---
+++ Assets/Scripts/Manager/MyGameTaskTimeManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Tasks;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Manager
+{
+    public class MyGameTaskTimeManager : NetworkBehaviour
+    {
+        public static MyGameNetWorkManager Instance { get; private set; }
+
+        public List<TaskUtil> AllTasks = new ();
+
+        #region MonoBehavior
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                return;
+            }
+
+            Instance = FindObjectOfType(typeof(MyGameNetWorkManager)) as MyGameNetWorkManager;
+            if (Application.isPlaying)
+            {
+                transform.SetParent(null);
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        #endregion
+    }
+}
