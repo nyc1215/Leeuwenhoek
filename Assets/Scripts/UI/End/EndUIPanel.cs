93b12cd23e724b95587ae1b993243247c69f2335
BILIBILIniyichao <niyichao@bilibili.com>
2022-09-07 19:03:56 +0800

    结束界面返回到主界面按钮

--- Assets/Scripts/UI/End/EndUIPanel.cs
+++ Assets/Scripts/UI/End/EndUIPanel.cs
@@ -1,3 +1,6 @@
+using System;
+using FairyGUI;
+using Manager;
 using UI.Util;
 using UnityEngine;
 
@@ -5,5 +8,18 @@ namespace UI.End
 {
     public class EndUIPanel : UIPanelUtil
     {
+        private GButton _quitButton;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _quitButton = GetButton("Button_Back");
+        }
+
+        private void Start()
+        {
+            _quitButton.onClick.Add(() => { UIOperationUtil.GoToScene(MyGameManager.Instance.uiJumpData.bootMenu); });
+        }
     }
 }
